0da5b87ca6eb28da4c6b78febf3a78c62c27aa01
Michal Artazov <michal@artazov.cz>
2020-09-21 17:12:21 +0200

    implement build scripts

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+	entry: ['@babel/polyfill', './src/index'],
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+		publicPath: "/",
+		libraryTarget: 'umd',
+	},
+	devtool: 'source-map',
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js', '.json'],
+	},
+	module: {
+		rules: [
+			{ test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+			{
+				test: /\.[tj]sx?$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				options: { presets: [require.resolve('@babel/preset-env')] },
+				enforce: 'post',
+			},
+		],
+	},
+}
